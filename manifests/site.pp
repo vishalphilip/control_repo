7103b36201a3a3a05b216e1b671ed15b9069bf7a
vishalphilip <vishalphilip@gmail.com>
2022-08-12 15:45:11 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a Readme',
-    owner   => 'root',
-   }
  }
+node 'master.puppet.vm'{
+  include role::master_server
+  }
